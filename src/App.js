5d3d3e521102586afab4c08f2c3bff8eb73c900f
KushalBasak2021 <kushalbasak14581@gmail.com>
2022-09-26 01:19:21 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import questions from "./questionData";
+import "./App.css";
 
 function App() {
+  const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
+  const [score, setScore] = useState(0);
+  function handleClick(isCorrect) {
+    if (isCorrect) {
+      setScore((prevScore) => prevScore + 1);
+    }
+    setCurrentQuestionNumber(
+      (previousQuestionNumber) => previousQuestionNumber + 1
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {currentQuestionNumber === 4 ? (
+        <div className="App conditional">
+          Your score is {score}/{questions.length}
+        </div>
+      ) : (
+        <div className="App">
+          <div className="left-section">
+            <h3>Question</h3>
+            <p>
+              {currentQuestionNumber + 1}/{questions.length}
+            </p>
+            <h4>{questions[currentQuestionNumber].questionText}</h4>
+          </div>
+          <div className="right-section">
+            {questions[currentQuestionNumber].answerOptions.map(
+              (answerOptions) => (
+                <button onClick={() => handleClick(answerOptions.isCorrect)}>
+                  {answerOptions.answerText}
+                </button>
+              )
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
